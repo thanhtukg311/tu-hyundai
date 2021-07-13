4784840b5e3a9996d991aab5ba0d52e2940a4dfb
dungbanhquang <65384664+dungbanhquang@users.noreply.github.com>
2021-07-13 22:34:15 +0700

    edit day03 + page noibat

--- src/views/products/Highlights.vue
+++ src/views/products/Highlights.vue
@@ -1,6 +1,119 @@
 <template>
     <div>
-        Nổi Bật
+        <section class="box01">
+                    <div class="container">
+                        <div class="tlt_center">
+                            <h3 class="tlt_h3">Không gian rộng rãi trong một thiết kế nhỏ gọn </h3>
+                            <span>Grand i10 sở hữu kích thước lớn hàng đầu phân khúc, đem đến một không gian thoải mái tối đa. Bên cạnh đó là sự tỉ mỉ, tinh tế trên các chi tiết cùng các tiện ích.</span>
+                        </div>
+                        <div class="fr_video">
+                            <img src="./../../assets/images/noibat_img01.jpg">
+                        </div>
+                    </div>
+                </section>
+                <!--box02-->
+                <section class="box02 bg_02">
+                    <div class="container">
+                        <div class="tlt_center">
+                            <h3 class="tlt_h3">Thiết kế đậm chất thể thao</h3>
+                            <span>Grand i10 All New thu hút mọi ánh nhìn trên mọi góc độ với các đường nét thời trang thể thao phá cách mang màu sắc của nghệ thuật đương đại
+                            </span>
+                        </div>
+                        <div class="img_center">
+                            <img src="./../../assets/images/noibat_img02.jpg">
+                        </div>
+                        <ul class="blog_col03">
+                            <li class="blog_box">
+                                <a href="#">
+                                    <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img03.jpg">
+                                    </p>
+                                    <p class="tlt_blog">
+                                        Cụm đèn ban ngày DRL (Daytime Running Light) được thiết kế phá cách dạng boomerang
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="blog_box">
+                                <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img04.jpg">
+                                    </p>
+                                <p class="tlt_blog">
+                                    Màn hình giải trí 8 inch
+                                </p>
+                            </li>
+                            <li class="blog_box">
+                                <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img05.jpg">
+                                    </p>
+                                <p class="tlt_blog">
+                                    2 túi khí cho hàng ghế trước
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <section class="box03">
+                    <div class="container">
+                        <div class="tlt_center">
+                            <h3 class="tlt_h3">Tiện nghi & an toàn trên từng cây số</h3>
+                            <span>Khoang nội thất của Grand i10 All New là tất cả những gì bạn cần. Đó là sự rộng rãi của không gian kết hợp cùng các tính năng an toàn vượt tầm phân khúc.
+                            </span>
+                        </div>
+                        <div class="img_center">
+                            <img src="./../../assets/images/noibat_img06.jpg">
+                        </div>
+                        <ul class="blog_col03">
+                            <li class="blog_box">
+                                <a href="#">
+                                    <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img07.jpg">
+                                    </p>
+                                    <p class="tlt_blog">
+                                        Cụm điểu chỉnh media tích hợp nhận diện giọng nói
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="blog_box">
+                                <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img08.jpg">
+                                    </p>
+                                <p class="tlt_blog">
+                                    Màn hình giải trí 8 inch
+                                </p>
+                            </li>
+                            <li class="blog_box">
+                                <p class="img_blog">
+                                        <img src="./../../assets/images/noibat_img09.jpg">
+                                    </p>
+                                <p class="tlt_blog">
+                                    2 túi khí cho hàng ghế trước
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <!--box03-->
+                <section class="box04">
+                    <div class="container">
+                        <div class="tlt_center">
+                            <h3 class="tlt_h3">Thư viện</h3>
+                        </div>
+                        <div class="cate_img">
+                            <img src="./../../assets/images/noibat_img06.jpg">
+                        </div>
+                        <p class="btn_r">
+                            <a class="btn_down" href="#"><span><img src="./../../assets/images/btn_download.png"></span>Tải Catalog</a>
+                        </p>
+                    </div>
+                </section>
+                <section class="prev-next-cate">
+                    <div class="container">
+                        <div class="flex_cate_bottom">
+                            <a class="cate_btn prev_cate" href="#"><i class="fal fa-chevron-left"></i>An toàn</a>
+                            <a class="cate_btn next_cate" href="#">Thông số xe<i class="fal fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </section>
     </div>
 </template>
 
