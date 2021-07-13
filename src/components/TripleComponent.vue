a45ce44f04d32eed0971de4949c3026f5b5589a0
dungbanhquang <65384664+dungbanhquang@users.noreply.github.com>
2021-07-13 22:50:36 +0700

    Update TripleComponent.vue

--- src/components/TripleComponent.vue
+++ src/components/TripleComponent.vue
@@ -1,17 +1,19 @@
 <template>
     <section class="box02">
         <div class="container">
-            <div class="blog_col03">
-                <div class="blog_box" v-for="(desc,index) in listDesc" :key="index">
+            <ul class="blog_col03">
+                <li class="blog_box" v-for="(desc,index) in listDesc" :key="index">
                     <a href="#">
-                        <img :src="desc.img" width="360px">
+                        <p class="img_blog">
+                            <img :src="desc.img">
+                        </p>
                         <p class="tlt_blog">
                             {{desc.title}}
                         </p>
                         <span>{{desc.desc}}</span>
                     </a>
-                </div>
-            </div>
+                </li>
+            </ul>
         </div>
     </section>
 </template>
