051ca34d993e6759cbc4ee9258fbaca561b3e637
thanhtukg311 <38935201+thanhtukg311@users.noreply.github.com>
2021-07-13 09:49:09 +0700

    update

--- src/services/jsonData.js
+++ src/services/jsonData.js
@@ -5,6 +5,11 @@ const products = [
         'name' : 'i10 Sedan',
         'description' : 'All New Grand',
         'img_url' : '/product/i10-ac3-highlights-kv-pc.jpg',
+        'utilities': {
+            'title_1': 'Công nghệ đỉnh cao tích hợp trong một chiếc xe nhỏ gọn',
+            'desc_1': 'Những chuyến hành trình của bạn sẽ trở nên thú vị với những công nghệ tiện ích được trang bị bên trong Grand i10',
+            'img_1': '/product/noi-that.jpg'
+        }
     },
     {
         'slug' : 'accent-2020',
