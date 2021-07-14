2c76635f83ba8a8bf9936fc212b53bef630509fe
thanhtukg311 <38935201+thanhtukg311@users.noreply.github.com>
2021-07-14 21:37:37 +0700

    update

---
+++ src/components/SlideImageComponent.vue
@@ -0,0 +1,33 @@
+<template>
+            <carousel :per-page="1" :mouse-drag="false" :autoplay="true"  :autoplayTimeout="2000" v-if="items.length > 0"  :navigationEnabled="false" :loop="true">
+                <slide v-for="(item, index) in items" :key="index">
+                    <img :src="item.img" alt="" title="" style="margin-bottom: 30px">
+                    <div class="tlt_center">
+                        <h3 class="tlt_h3">{{item.title}}</h3>
+                        <span>{{item.desc}}</span>
+                    </div>
+                </slide>
+            </carousel>
+</template>
+
+<script>
+    import { Carousel, Slide } from 'vue-carousel';
+    export default {
+        name: "SlideImageComponent",
+        components: {Carousel, Slide},
+        props: {
+            items: {
+                type: Array,
+                default: () => []
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .owl-pagination {
+        position: absolute;
+        right: 0;
+        top: -50px;
+    }
+</style>
