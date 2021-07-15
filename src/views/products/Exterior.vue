6f790d388126040e29c597dcb5b54cafcd0aaa85
thanhtukg311 <38935201+thanhtukg311@users.noreply.github.com>
2021-07-15 23:35:44 +0700

    update

--- src/views/products/Exterior.vue
+++ src/views/products/Exterior.vue
@@ -5,121 +5,63 @@
                 <title-component :desc="prod.exterior.desc" :title="prod.exterior.title"/>
             </div>
         </section>
-        <!--box02-->
-                <div class="item_box_tgle on">
-                    <div class="item_toggle">
-                        <img src="./../../assets/images/item_img01.png">
-                        <p class="tlt_item">Mặt trước</p>
-                        <button class="btnShow"></button>
-                    </div>
-                    <div id="item_anchor" class="item_content container">
-                        <div class="transition">
-                            <div class="tlt_center">
-                                <h3 class="tlt_h3">Mặt trước thiết kế thể thao mạnh mẽ</h3>
-                                <span>Kích thích thị giác bằng lưới tản nhiệt thác nước mới cùng cụm đèn ban ngày DRL thiết kế phá cách dạng boomerang và đèn sương mù.
-                                </span>
-                            </div>
-                            <div class="img_center">
-                                <img src="./../../assets/images/ngoaithat_img01.jpg">
-                            </div>
-                            <ul class="blog_col02">
-                                <li class="blog_box">
-                                    <a href="#">
-                                        <p class="img_blog">
-                                            <img src="./../../assets/images/ngoaithat_img02.jpg">
-                                        </p>
-                                        <p class="tlt_blog">
-                                            Cụm đèn ban ngày DRL (Daytime Running Light) được thiết kế phá cách dạng boomerang
-                                        </p>
-                                    </a>
-                                </li>
-                                <li class="blog_box">
-                                    <p class="img_blog">
-                                        <img src="./../../assets/images/ngoaithat_img03.jpg">
-                                    </p>
-                                    <p class="tlt_blog">
-                                        Đèn sương mù tích hợp trên cản trước thể thao
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+        <div class="item_box_tgle" v-bind:class="{'on': isOpen}">
+            <div class="item_toggle">
+                <img src="./../../assets/images/item_img01.png">
+                <p class="tlt_item">Mặt trước</p>
+                <button class="btnShow" v-on:click="myFilter"></button>
+            </div>
+            <div id="item_anchor" class="item_content container">
+                <div class="transition">
+                    <title-component :desc="prod.exterior.main_desc" :title="prod.exterior.main_title"/>
+                    <image-center-component :img-url="prod.exterior.main_img"/>
+                    <double-component :list-desc="prod.exterior.list_desc"/>
                 </div>
-                <div class="item_box_tgle on">
-                    <div class="item_toggle">
-                        <img src="./../../assets/images/item_img02.png">
-                        <p class="tlt_item">Cạnh bên</p>
-                        <button class="btnShow"></button>
-                    </div>
-                    <div class="item_content container">
-                        <ul class="blog_col03">
-                            <li class="blog_box">
-                                <a href="#">
-                                    <p class="img_blog">
-                                        <img src="./../../assets/images/ngoaithat_img04.jpg">
-                                    </p>
-                                    <p class="tlt_blog">
-                                        Gương chiếu hậu gập điện tích hợp sưởi gương
-                                    </p>
-                                </a>
-                            </li>
-                            <li class="blog_box">
-                                <p class="img_blog">
-                                    <img src="./../../assets/images/ngoaithat_img05.jpg">
-                                </p>
-                                <p class="tlt_blog">
-                                    Tay nắm cửa mạ chrome
-                                </p>
-                            </li>
-                            <li class="blog_box">
-                                <p class="img_blog">
-                                    <img src="./../../assets/images/ngoaithat_img06.jpg">
-                                </p>
-                                <p class="tlt_blog">
-                                    Vành hợp kim 15 inch cao cấp tạo hình trẻ trung 
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="item_box_tgle on">
-                    <div class="item_toggle">
-                        <img src="./../../assets/images/item_img03.png">
-                        <p class="tlt_item">Mặt sau</p>
-                        <button class="btnShow"></button>
-                    </div>
-                    <div class="item_content container">
-                        <ul class="blog_col02">
-                            <li class="blog_box">
-                                <a href="#">
-                                    <p class="img_blog">
-                                        <img src="./../../assets/images/ngoaithat_img07.jpg">
-                                    </p>
-                                    <p class="tlt_blog">
-                                        Đèn hậu dạng LED thiết kế cách điệu, đồng bộ với cụm đèn phía trước
-                                    </p>
-                                </a>
-                            </li>
-                            <li class="blog_box">
-                                <p class="img_blog">
-                                    <img src="./../../assets/images/ngoaithat_img08.jpg">
-                                </p>
-                                <p class="tlt_blog">
-                                    Đèn phanh trên cao
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            </div>
+        </div>
+        <div class="item_box_tgle" v-bind:class="{'on': isOpen}">
+            <div class="item_toggle">
+                <img src="./../../assets/images/item_img02.png">
+                <p class="tlt_item">Cạnh bên</p>
+                <button class="btnShow" v-on:click="myFilter"></button>
+            </div>
+            <div class="item_content container">
+                <triple-component :list-desc="prod.exterior.list_desc2"/>
+            </div>
+        </div>
+        <div class="item_box_tgle" v-bind:class="{'on': isOpen}">
+            <div class="item_toggle">
+                <img src="./../../assets/images/item_img03.png">
+                <p class="tlt_item">Mặt sau</p>
+                <button class="btnShow" v-on:click="myFilter"></button>
+            </div>
+            <div class="item_content container">
+            <double-component :list-desc="prod.exterior.list_desc3"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import TitleComponent from "@/components/TitleComponent";
+    import DoubleComponent from "@/components/DoubleComponent";
+    import ImageCenterComponent from "@/components/ImageCenterComponent";
+    import TripleComponent from "@/components/TripleComponent";
+
     export default {
         name: "Exterior",
-        components: {TitleComponent},
-        props:['prod']
+        components: {TripleComponent, ImageCenterComponent, DoubleComponent, TitleComponent},
+        props: ['prod'],
+        data() {
+            return{
+                isOpen: {}
+            }
+        },
+        methods: {
+            myFilter: function() {
+                this.isOpen = !this.isOpen;
+            }
+        }
     }
 </script>
 
