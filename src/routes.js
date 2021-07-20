85d10d7a7a47b4c21cf7c9c9f077000bc2a71477
thanhtukg311 <38935201+thanhtukg311@users.noreply.github.com>
2021-07-20 20:03:17 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProductLayout from "@/layouts/ProductLayout";
 Vue.use(VueRouter)
 
 export const router = new VueRouter({
+    mode: 'history',
     routes: [
         {
             path: '/',
